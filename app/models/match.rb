23ff771e3be803655428fb620dd8035142c576ac
Ariani Plukaard <ari.plukaard@gmail.com>
2022-12-02 10:45:21 +1100

    turbo ajax stuff in progress

--- app/models/match.rb
+++ app/models/match.rb
@@ -8,37 +8,14 @@ class Match < ApplicationRecord
   validates_uniqueness_of :matchee_id, { scope: :matcher_id }
   validates :matchee_id, comparison: { other_than: :matcher_id }
 
-  # broadcasts_to ->(match) { [match.matchee, :matches] }, inserts_by: :prepend, target: "user_#{matchee_id}_matches"
-  # after_create_commit :broadcast
-  # after_create_commit -> { broadcast_prepend_to helpers.dom_id(matchee, :matches), partial: "matches/match", locals: { match: self }, target: helpers.dom_id(matchee, :matches) }
-
-  # For create, currrent_user is matcher
+    # For create, currrent_user is matcher
   after_create_commit -> { broadcast_prepend_to "user_#{matchee.id}_pending_matches", partial: "matches/match", locals: { match: self }, target: "pending_matches" }
+  # after_create_commit -> { broadcast_remove_to "user_#{matcher.id}_pending_matches" }
+
+  # For update, currrent_user is matchee
   after_update_commit -> { broadcast_remove_to "user_#{matchee.id}_pending_matches" }
-  after_update_commit -> { broadcast_prepend_to "user_#{matchee.id}_pending_matches", partial: "matches/match", locals: { match: self }, target: "pending_matches" }
+  # after_update_commit -> { broadcast_prepend_to "user_#{matcher.id}_pending_matches", partial: "matches/match", locals: { match: self }, target: "pending_matches" }
 
-  after_update_commit -> { broadcast_remove_to "user_#{matcher.id}_pending_matches" }
-  after_update_commit -> { broadcast_prepend_to "user_#{matcher.id}_pending_matches", partial: "matches/match", locals: { match: self }, target: "pending_matches" }
-
-  # private
-  # def broadcast
-  #   broadcast_prepend_to matchee, :matches
-  # end
-
+  # after_update_commit -> { broadcast_prepend_to "user_#{matchee.id}_pending_matches", partial: "matches/match", locals: { match: self }, target: "pending_matches" }
+  # after_update_commit -> { broadcast_remove_to "user_#{matcher.id}_pending_matches" }
 end
-
-# Match.statuses
-# match = Match.find(3)
-# math.update(status: "accepted")
-
-# class Clearance < ApplicationRecord
-  #     belongs_to :petitioner, class_name: "Contact"
-  #     belongs_to :examiner,   class_name: "User"
-  #
-  #     after_create_commit :broadcast_later
-  #
-  #     private
-  #       def broadcast_later
-  #         broadcast_prepend_later_to examiner.identity, :clearances
-  #       end
-  #   end
