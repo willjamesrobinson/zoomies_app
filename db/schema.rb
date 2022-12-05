98f55affc8503761842ea032a71bde1c6510a00c
will robinson <willjamesr@gmail.com>
2022-12-05 21:55:19 +1100

    made cards for doggy dates

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_003844) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_040528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_003844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "message_id"
-    t.date "date"
     t.bigint "match_id", null: false
     t.index ["match_id"], name: "index_doggy_dates_on_match_id"
     t.index ["message_id"], name: "index_doggy_dates_on_message_id"
