00c3394893eb968805673eb2fd59dc0ee53b2dac
Ariani Plukaard <ari.plukaard@gmail.com>
2022-12-08 10:41:51 +1100

    updated header navbar thing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
+  get "/about-us", to: "pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
